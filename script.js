05890968833d8e2e631a6f5644703aa91d617c08
ArthurM.Torres <arthur.dev.torres@gmail.com>
2026-05-27 00:20:02 -0300

    Exercícios Lógica JS 01 ao 06

---
+++ script.js
@@ -0,0 +1,48 @@
+//-------------------------------------------
+console.log("Olá Mundo");
+const cidade = "Muriaé";
+let idade = 22;
+let nome  = "Arthur M.Torres";
+let curso = "Análise e Desenvolvimento de Sistemas";
+let periodo = "1º Período";
+console.log(nome);
+console.log(curso,"-",periodo);
+let mensagem ="Estou aprendendo JavaScript";
+//-------------------------------------------
+
+console.log("\nExercício 06\n");
+let produto = "Mouse";
+let preco = 59.90;
+let quantidade = 2;
+console.log(produto,"\n"+ preco,"\n"+quantidade,"\n");
+
+//-------------------------------------------
+
+console.log("Exercício 07\n");
+console.log("Produto:",produto+"\n"+"Preço:",preco+"\n"+"Quantidade:",quantidade);
+
+//-------------------------------------------
+
+console.log("Exercício 08\n");
+let saldo = 100;
+saldo = 150
+console.log(saldo);
+
+//-------------------------------------------
+
+console.log("Exercício 09\n");
+const PI = 3.14;
+let raio = 5;
+console.log("PI:",PI+"\n"+"raio:",raio,"\n");
+
+//-------------------------------------------
+
+console.log("Exercício 10\n");
+let matricula = "00000001";
+let nota = 9.0;
+console.log("Nome:",nome);
+console.log("Curso:",curso);
+console.log("Matrícula:",matricula);
+console.log("Nota:",nota,"\n");
+
+//-------------------------------------------
