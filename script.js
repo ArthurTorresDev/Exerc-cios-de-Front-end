c1e45412e5d35156e9e959a0e7e62f046204c55b
ArthurM.Torres <arthur.dev.torres@gmail.com>
2026-05-27 14:23:01 -0300

    Exercícios de Lógica JS 1 ao 20

--- script.js
+++ script.js
@@ -45,4 +45,70 @@ console.log("Curso:",curso);
 console.log("Matrícula:",matricula);
 console.log("Nota:",nota,"\n");
 
-//-------------------------------------------
+//-----------------------------------------
+
+console.log("Execício 11\n");
+let a = 3;
+let b =3;
+console.log(a+b,"\n");
+
+//----------------------------------------
+
+console.log("Execício 12\n");
+console.log(a-b,"\n");
+
+//---------------------------------------
+
+console.log("Execícios 13\n");
+console.log(a*b,"\n");
+
+//---------------------------------------
+
+console.log("Exercício 14\n");
+console.log(a/b,"\n");
+
+//---------------------------------------
+
+console.log("Exercício 15\n");
+let numero = 10;
+console.log(2*numero,"\n");
+
+//---------------------------------------
+
+console.log("Exercício 16");
+numero = 21
+console.log(3*numero,"\n");
+
+//---------------------------------------
+
+console.log("Exercício 17");
+preco = 100;
+let desconto = 20;
+console.log(preco-desconto,"\n");
+
+//---------------------------------------
+console.log("Exercício 18\n");
+let nota1 = 7;
+let nota2 = 8;
+let nota3 = 9;
+let calculo = ((nota1+nota2+ nota3)/3);
+console.log("media das noatas:",calculo,"\n");
+//---------------------------------------
+console.log("Exercício 19\n");
+let minutos = 120;
+let conversorHoras = (minutos/60);
+console.log(conversorHoras,"\n");
+//---------------------------------------
+console.log("Exercício 20\n");
+let dias = 7;
+let resultado = (7 * 24);
+console.log("7 dias são equivalentes a",resultado,"horas\n");
+//---------------------------------------
+console.log("Exercício 21\n");
+//---------------------------------------
+console.log("Exercício 22\n");
+
+//---------------------------------------
+console.log("Exercício 23\n");
+//---------------------------------------
+console.log("Exercício 24\n");
